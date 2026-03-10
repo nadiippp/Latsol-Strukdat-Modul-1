59258964a2236bbf3cad67289982c3821415b5a2
nadiippp <5027251084@student.its.ac.id>
2026-03-10 16:10:19 +0700

    virtual

--- soal3.cpp
+++ soal3.cpp
@@ -6,4 +6,19 @@ class Pemain{
         string nama;
         int koin, exp, tt;
     
+    public:
+        Pemain(string namaPemain, int ttPemain){
+            nama = namaPemain;
+            tt = ttPemain;
+            koin = 0;
+            exp = 0;
+        }
+    
+        virtual void hunt() = 0;
+
+        virtual void info(){
+            cout << "Nama : " << nama << endl;
+            cout << "Koin : " << koin << endl;
+            cout << "EXP : " << exp << endl;
+        }
 };
