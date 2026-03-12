0ee16e1beca8fa2df9644b61b4ab0165ae17e5d6
nadiippp <5027251084@student.its.ac.id>
2026-03-12 13:43:26 +0700

    derived class

--- soal3.cpp
+++ soal3.cpp
@@ -14,11 +14,20 @@ class Pemain{
             exp = 0;
         }
     
-        virtual void hunt() = 0;
+        virtual void hunt(int baseKoin, int baseExp) = 0;
 
         virtual void info(){
             cout << "Nama : " << nama << endl;
             cout << "Koin : " << koin << endl;
             cout << "EXP : " << exp << endl;
+        }
+};
+
+class PemainSolo : public Pemain{
+    public:
+        PemainSolo(string namaPemain, int ttPemain) : Pemain(namaPemain, ttPemain){}
+        void hunt(int baseKoin, int baseExp) override{
+           koin += baseKoin;
+           exp += baseExp * (1 + tt); 
         }
 };
